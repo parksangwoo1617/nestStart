bce354f5765ffbf6d253b84433c4aa91f9c09bae
parksangwoo <b01056721617@gmail.com>
2021-04-10 21:25:57 +0900

    nestjs

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity'
+import { CreateMovieDto } from './dto/create-movie.dto'
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) {}
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get(":id")
+    getOne(@Param("id") id: number): Movie{
+        console.log(typeof id);
+        return this.moviesService.getOne(id);
+    }
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete(":id")
+        delete(@Param('id') id: number) {
+            return this.moviesService.deleteOne(id);
+    }
+
+    @Patch(':id')
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(id, updateData);
+    }
+}
